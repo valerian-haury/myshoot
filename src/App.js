bbbd5441d6152ec8336bb2b5cd2546c1671d9638
Valérian Haury <haury.valerian@gmail.com>
2021-07-07 12:36:21 +0200

    add router and default route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+
 import './App.css';
 
-import { ShootingProgramsList } from "./features/shootingPrograms/ShootingProgramsList";
+import {ShootingProgramsList} from "./features/shootingPrograms/ShootingProgramsList";
 
 function App() {
-  return (
-    <div className="App">
-      <ShootingProgramsList />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => (
+                            <>
+                                <ShootingProgramsList/>
+                            </>
+                        )}
+                    />
+                    {/*<Route exact path="/posts/:postId" component={SinglePostPage} />*/}
+                    <Redirect to="/"/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
